b2beb729ec036fa517242f4f5dba4288da164a7f
skymiri <hnk0225@gmail.com>
2023-10-03 13:51:32 -0700

    Add buttons on Calulator.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./index.css";
+import Calculator from "./calulator.js";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
